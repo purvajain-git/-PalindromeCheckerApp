78f72c99a1128c54b4c5c9a025c37d82a8b39c83
purvajain-git <pj8602@srmist.edu.in>
2026-02-25 16:16:07 +0530

    UC8

--- src/UseCasePalindromeCheckerApp.java
+++ src/UseCasePalindromeCheckerApp.java
@@ -1,22 +1,21 @@
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.LinkedList;
 
 public class UseCasePalindromeCheckerApp {
 
   public static void main(String[] args) {
 
-    String input = "refer";
+    String input = "level";
 
-    Deque<Character> deque = new ArrayDeque<>();
+    LinkedList<Character> list = new LinkedList<>();
 
     for (char c : input.toCharArray()) {
-      deque.add(c);
+      list.add(c);
     }
 
     boolean isPalindrome = true;
 
-    while (deque.size() > 1) {
-      if (!deque.removeFirst().equals(deque.removeLast())) {
+    while (list.size() > 1) {
+      if (!list.removeFirst().equals(list.removeLast())) {
         isPalindrome = false;
         break;
       }
