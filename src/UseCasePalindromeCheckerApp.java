d35eeb0d9fac0b21e1744d950e2cd71c50bb80de
purvajain-git <pj8602@srmist.edu.in>
2026-02-25 16:33:39 +0530

    UC13

--- src/UseCasePalindromeCheckerApp.java
+++ src/UseCasePalindromeCheckerApp.java
@@ -1,29 +1,36 @@
-
-public class UseCasePalindromeCheckerApp {
-    public static void main(String[] args) {
-        String input = "level";
-        PalindromeStrategy strategy = new StackStrategy();
-        boolean isPalindrome = strategy.check(input);
-        System.out.println("Input : " + input);
-        System.out.println("Is Palindrome? : " + isPalindrome);
-    }
-}
+import java.util.Stack;
 
 interface PalindromeStrategy {
     boolean check(String input);
 }
 
 class StackStrategy implements PalindromeStrategy {
+    @Override
     public boolean check(String input) {
-        java.util.Stack<Character> stack = new java.util.Stack<>();
+        if (input == null) return false;
+        Stack<Character> stack = new Stack<>();
         for (char c : input.toCharArray()) {
             stack.push(c);
         }
-        for (char c : input.toCharArray()) {
-            if (c != stack.pop()) {
-                return false;
-            }
+        StringBuilder reversed = new StringBuilder();
+        while (!stack.isEmpty()) {
+            reversed.append(stack.pop());
         }
-        return true;
+        return input.equalsIgnoreCase(reversed.toString());
+    }
+}
+
+public class UseCasePalindromeCheckerApp {
+    public static void main(String[] args) {
+        String input = "level";
+        PalindromeStrategy strategy = new StackStrategy();
+
+        long startTime = System.nanoTime();
+        boolean isPalindrome = strategy.check(input);
+        long endTime = System.nanoTime();
+
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
+        System.out.println("Execution Time : " + (endTime - startTime) + " ns");
     }
 }
