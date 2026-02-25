f9a52e4827c3f01605fc6adc75e1d890cf55d2f9
purvajain-git <pj8602@srmist.edu.in>
2026-02-25 14:54:44 +0530

    UC1

--- src/UseCasePalindromeCheckerApp.java
+++ src/UseCasePalindromeCheckerApp.java
@@ -1,5 +1,8 @@
 public class UseCasePalindromeCheckerApp {
   public   static void main(String[] args) {
-      System.out.println("Hello Palindrome");
+      System.out.println("Welcome to the Palindrome Checker Management System");
+      System.out.println("Version : 1.0");
+      System.out.println("System initialized successfully.");
     }
 }
+
