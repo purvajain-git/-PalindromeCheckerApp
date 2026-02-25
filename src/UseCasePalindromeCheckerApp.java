b407cfc9013b26ffa3ce14724f8e55858a2e38de
purvajain-git <pj8602@srmist.edu.in>
2026-02-25 16:09:11 +0530

    UC7

--- src/UseCasePalindromeCheckerApp.java
+++ src/UseCasePalindromeCheckerApp.java
@@ -1,25 +1,22 @@
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Stack;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 public class UseCasePalindromeCheckerApp {
 
   public static void main(String[] args) {
 
-    String input = "civic";
+    String input = "refer";
 
-    Queue<Character> queue = new LinkedList<>();
-    Stack<Character> stack = new Stack<>();
+    Deque<Character> deque = new ArrayDeque<>();
 
     for (char c : input.toCharArray()) {
-      queue.add(c);
-      stack.push(c);
+      deque.add(c);
     }
 
     boolean isPalindrome = true;
 
-    while (!queue.isEmpty()) {
-      if (!queue.poll().equals(stack.pop())) {
+    while (deque.size() > 1) {
+      if (!deque.removeFirst().equals(deque.removeLast())) {
         isPalindrome = false;
         break;
       }
