b6dee84e3e928998beab971da3df6a756c0345ab
purvajain-git <pj8602@srmist.edu.in>
2026-02-25 15:47:35 +0530

    UC3

--- src/UseCasePalindromeCheckerApp.java
+++ src/UseCasePalindromeCheckerApp.java
@@ -1,8 +1,35 @@
-public class UseCasePalindromeCheckerApp {
-  public   static void main(String[] args) {
-      System.out.println("Welcome to the Palindrome Checker Management System");
-      System.out.println("Version : 1.0");
-      System.out.println("System initialized successfully.");
-    }
-}
+import java.util.Scanner;
 
+public class UseCasePalindromeCheckerApp {
+
+
+  public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("Enter a string to check: ");
+    String input = scanner.nextLine();
+
+
+    String reversed = "";
+
+
+    for (int i = input.length() - 1; i >= 0; i--) {
+      reversed += input.charAt(i);
+    }
+
+
+    System.out.println("Original String: " + input);
+    System.out.println("Reversed String: " + reversed);
+
+
+    if (input.equalsIgnoreCase(reversed)) {
+      System.out.println("Result: The string IS a palindrome.");
+    } else {
+      System.out.println("Result: The string IS NOT a palindrome.");
+    }
+
+
+    scanner.close();
+  }
+}
